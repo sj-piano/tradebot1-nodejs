71b9ea86c25e00c280df84273e179c8593cc2241
StJohn Piano <stjohnpiano@gmail.com>
2020-11-29 22:20:24 +0000

    src dir and TradeBot.js.

---
+++ src/TradeBot.js
@@ -0,0 +1,23 @@
+class TradeBot {
+
+
+	constructor() {
+		this.name = 'TradeBot';
+	}
+
+
+	async z() {
+		try {
+			console.log('Location: z');
+			return 'z';
+		} catch(err) {
+			throw(err);
+		}
+	}
+
+
+}
+
+
+
+module.exports = TradeBot;
