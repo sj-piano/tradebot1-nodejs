1f19b3a8f9cf523f1adb0d048972958e263a67df
StJohn Piano <stjohnpiano@gmail.com>
2020-11-30 16:18:10 +0000

    Finish gasPrice test.

--- test/test_TradeBot.js
+++ test/test_TradeBot.js
@@ -1,22 +1,32 @@
 let assert = require('assert');
+let _ = require('lodash');
+
+
+/* START SHORTCUTS */
 let log = console.log;
+let aq = assert.equal;
+/* END SHORTCUTS */
 
 
 describe('TradeBot', function() {
 
+	let t;
+
+	before(function() {
+		t = new TradeBot();
+	});
+
 	describe('#test_name', function() {
 		it('should return "TradeBot".', function() {
-			tb = new TradeBot();
-			assert.equal(tb.name, 'TradeBot');
+			assert.equal(t.name, 'TradeBot');
 		});
 	});
 
 	describe('#test_getGasPrice', function() {
-		it('should return a number.', async function() {
-			t = new TradeBot();
-			x = await t.makeRequest();
-			log(x);
-			//assert.equal(x, 'z');
+		it('should return an object with an "average" value.', async function() {
+			x = await t.getGasPriceETH();
+			aq(_.has(x, 'average'), true);
+			aq(_.isNumber(x.average), true);
 		});
 	});
 
