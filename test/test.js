c1c58cb63589134820bdaa50152157181966a00f
StJohn Piano <stjohnpiano@gmail.com>
2020-11-30 15:59:38 +0000

    Delete extra tests.

--- test/test.js
+++ test/test.js
@@ -8,24 +8,3 @@ describe('Array', function() {
     });
   });
 });
-
-
-describe('Bar', function() {
-	describe('#test_attribute', function() {
-		it('should have an attribute "x" with a value of "foo".', function() {
-			b = new Bar();
-			assert.equal(b.x, 'foo');
-		});
-	});
-});
-
-
-describe('TradeBot', function() {
-	describe('#test_z', function() {
-		it('should return "z".', async function() {
-			tb = new TradeBot();
-			x = await tb.z();
-			assert.equal(x, 'z');
-		});
-	});
-});
