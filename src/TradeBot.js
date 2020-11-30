b30a19e5b4d0761d2aa5736ac7e8108d8e073c56
StJohn Piano <stjohnpiano@gmail.com>
2020-11-30 16:00:03 +0000

    Use node-fetch.

--- src/TradeBot.js
+++ src/TradeBot.js
@@ -1,3 +1,7 @@
+//let fetch = require('fetch');
+const fetch = require('node-fetch');
+
+
 class TradeBot {
 
 
@@ -13,6 +17,23 @@ class TradeBot {
 		} catch(err) {
 			throw(err);
 		}
+	}
+
+
+	async makeRequest() {
+		const params = {
+			Symbol: "tETHUSD",
+			Precision: "P0",
+		};
+		const url = `https://api.stg.deversifi.com/bfx/v2/book/${params.Symbol}/${params.Precision}`;
+		const response = await fetch(url, {
+			method: "GET",
+			headers: {
+			"Accept": "application/json",
+			"Content-Type": "application/json",
+			},
+		});
+		return response.json();
 	}
 
 
