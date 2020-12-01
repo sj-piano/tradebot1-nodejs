e3f294ad4071d2fe7cdcdf9e61a365edb5276cb2
StJohn Piano <stjohnpiano@gmail.com>
2020-12-01 13:42:25 +0000

    Expand runTradeBot.js

--- runTradeBot.js
+++ runTradeBot.js
@@ -1,35 +1,63 @@
+/* DESCRIPTION
+- Run a simple market-making TradeBot using NodeJS.
+- Market: Ethereum / United States Dollars (ETHUSD)
+- Venue: Deversifi
+*/
 
 
+const program = require('commander');
+const TradeBot = require('./src').TradeBot;
 
-console.log('test');
 
+// Shortcuts
+let log2 = console.log;
+
+
+// Error handling
 
 process.on('uncaughtException', (err) => {
-	console.error('There was an uncaught error', err)
-	process.exit(1) //mandatory (as per the Node docs)
-})
-
-process.on('unhandledRejection', (error, p) => {
-	console.log('=== UNHANDLED REJECTION ===');
-	console.log(error.stack);
-	console.log(p);
+	log2('=== UNCAUGHT EXCEPTION ===');
+	console.error(err)
+	process.exit(1);
 });
 
-//Promise.reject(new Error('not OK'))
+process.on('unhandledRejection', (err, rejectedPromise) => {
+	log2('=== UNHANDLED REJECTION ===');
+	console.error(err.stack);
+	//log(rejectedPromise);
+	process.exit(1);
+});
+
+// Test error handling
+//Promise.reject(new Error('promiseFoo'))
+//throw Error('errorFoo');
+
+
+// Command-line arguments
+program
+	.option('-d, --debug', 'Print out debug information.', false)
+	.option('-l, --logLevel <logLevel>', 'Set logging level.', 'error')
+	.option('-t, --timestamp', 'Include timestamp in log messages', false)
+program.parse(process.argv);
+let debug = program.debug;
+let logLevel = program.logLevel;
+let timestamp = program.timestamp;
 
 
 
+// Run main top-level function
+main();
 
-foo();
 
-async function foo() {
+// Top-level function
+async function main() {
 	try {
-
-
+		tradeBotArgs = {debug, logLevel, timestamp, loggingOn:true};
+		t = new TradeBot(tradeBotArgs);
+		await t.init();
+		t.run();
 	} catch(err) {
-		console.log(err);
-		process.exit(0);
-		//console.log(err.response.body);
-		console.log('end');
+		console.error(err);
+		process.exit(1);
 	}
 }
