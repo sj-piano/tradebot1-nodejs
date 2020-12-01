f6e4f32aac538149f276be4a52c2ac8ba4732fc2
StJohn Piano <stjohnpiano@gmail.com>
2020-12-01 13:42:10 +0000

    Expand test_TradeBot.js

--- test/test_TradeBot.js
+++ test/test_TradeBot.js
@@ -1,19 +1,20 @@
-let assert = require('assert');
-let _ = require('lodash');
+const assert = require('assert');
+const _ = require('lodash');
 
 
-/* START SHORTCUTS */
-let log = console.log;
-let aq = assert.equal;
-/* END SHORTCUTS */
+// Shortcuts
+const log2 = console.log;
+const aq = assert.equal;
 
 
 describe('TradeBot', function() {
 
 	let t;
 
-	before(function() {
-		t = new TradeBot();
+	before(async function() {
+		const tradeBotArgs = {debug: false, timestamp: false, loggingOn: false}
+		t = new TradeBot(tradeBotArgs);
+		await t.init();
 	});
 
 	describe('#test_name', function() {
@@ -27,6 +28,12 @@ describe('TradeBot', function() {
 			x = await t.getGasPriceETH();
 			aq(_.has(x, 'average'), true);
 			aq(_.isNumber(x.average), true);
+		});
+	});
+
+	describe('#test_getBestBidAndAskETH', function() {
+		it('should return an object with a "bid" value and an "ask" value.', async function() {
+			x = await t.getBestBidAndAskETH();
 		});
 	});
 
